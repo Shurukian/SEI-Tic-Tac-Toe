e7b67aabe14375886e266295cd5d07e5a27b6ee2
shurukian <dvalentine03@gmail.com>
2020-10-23 23:31:36 -0400

    Created the gameboard
    Created the game board using query selectors, the .forEach method a
    eventlisteners on the data-cell attributes. Currently the board will
    only log that a cell has been clicked within the console.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -26,7 +26,7 @@ $(() => {
   $('#get-games').on('submit', gameEvents.onGetGames)
 
   // all of the event listeners for the game board
-  $('#box0').on('click', gameEvents.box0)
+  $('#box0').on('click', gameEvents.dataCells)
   // $('#box1').on('click', gameEvents.box1)
   // $('#box2').on('click', gameEvents.box2)
   // $('#box3').on('click', gameEvents.box3)
--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -14,13 +14,49 @@ const onNewGame = function (event) {
     .then(ui.onNewGameSuccess)
     .catch(ui.onNewGameFailure)
 }
-// ability to make a move on the board
-const box0  = function () {
 
-}
-// variables for the players
+// need variables for the players for the game
+const playerX = 'X'
+const playerO = 'O'
+
 // function to show the variable change between players.
+// const changePlayer = function () {
+//   playerX = !playerO
+// }
+
+// we want to have the ability to select all of the data-cell-index divs
+// I was able to find how to target the cells in an Array easier at the link
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+const dataCells = document.querySelectorAll('[data-cell-index]')
+
 // selector to click on the box and make it an X or an O
+// need to be able to loop through all of the cells to play the game
+// This will access all of the boxes in the array for the game board and allow
+// a click to happen on the dataCells variable one time. Found this on StackOverFlow :
+// https://stackoverflow.com/questions/38781349/how-to-make-onclick-event-to-work-only-once
+
+dataCells.forEach(cell => {
+  // I am very proud of this piece of code!
+  cell.addEventListener('click', cellClick, { once: true })
+})
+
+// ability to make a move on the board
+// ability to switch between turns for X and O
+// Determine whether there is a win, loss or draw
+function cellClick (event) {
+  const cell = event.target
+  const currentPlayer = playerX ? playerO : playerX
+
+// takeTurn is a fuction for the currentPlayer (playerX or playerO) to be able
+// to make a move on the board.
+  takeTurn(cell, currentPlayer)
+  changePlayer()
+}
+
+const takeTurn = function (cell, currentPlayer) {
+  cell.playerList.add(currentPlayer)
+}
+
 // arrays for the win conditions of the games
 const winCondition = [
 // horizontal cells
